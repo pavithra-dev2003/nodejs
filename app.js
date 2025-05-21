394635864a656c924e23fb7cdcb35578aafc6d7d
pavithra <pavethrapavethra588@gmail.com>
2025-05-21 20:12:11 +0530

    code changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 // Define a route
 app.get('/', (req, res) => {
-  res.send('Hello, its new instruction in image');
+  res.send('Hello, its new instruction');
 });
 
 // Start the server
